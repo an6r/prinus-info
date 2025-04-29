920ba6d50ed7d99438f8446e9cb7f7fd716f31c2
Maria Prinus <maryprinus@gmail.com>
2025-04-28 17:37:04 -0700

    Add translation support for the Home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,26 @@
-import Hero from '@/components/hero';
+import initTranslations from '@/app/i18n';
 
-export default function Home() {
+import Hero from '@/components/hero';
+import TranslationsProvider from '@/components/translations-provider';
+
+const i18nNamespaces = ['home'];
+
+export default async function Home({
+    params,
+}: {
+    params: Promise<{ locale: string }>;
+}) {
+    const { locale } = await params;
+    const { resources } = await initTranslations(locale, i18nNamespaces);
+
     return (
-        <>
+        <TranslationsProvider
+            namespaces={i18nNamespaces}
+            locale={locale}
+            resources={resources}
+        >
             <Hero />
             {/*<BlogItems/>*/}
-        </>
+        </TranslationsProvider>
     );
 }
