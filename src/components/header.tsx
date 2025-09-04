4f377e12acdb16e9d7c151b5e6799707476652e6
Maria Prinus <maryprinus@gmail.com>
2025-09-03 19:50:06 -0700

    Update logo with an image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,22 +3,14 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import { geom } from '@/fonts';
+import prinus_logo from '@/public/prinus-logo.png';
 
 function Header() {
     return (
         <header>
-            <div className={geom.className + ' logo'}>
-                <Link href="/" className="logo-text">
-                    <span>Prinus</span>
-                    <Image
-                        src="/logo.png"
-                        alt="Logo Tree"
-                        width={80}
-                        height={60}
-                    />
-                </Link>
-            </div>
+            <Link href="/" className="logo">
+                <Image src={prinus_logo} alt="Prinus Logo" width={300} />
+            </Link>
         </header>
     );
 }
